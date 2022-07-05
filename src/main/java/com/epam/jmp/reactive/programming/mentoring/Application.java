bd0958a20db1a88de0506d7f061a5c376957e19b
Stanislau_Sukora <stanislau.sukora@ukg.com>
2022-07-05 19:44:01 +0300

    Initial commit

---
+++ src/main/java/com/epam/jmp/reactive/programming/mentoring/Application.java
@@ -0,0 +1,13 @@
+package com.epam.jmp.reactive.programming.mentoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
